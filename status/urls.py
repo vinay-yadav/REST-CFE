d26bdb6d6cd4d8591bfee13c52b72b59c4880677
Vinay Yadav <yadav.vinay1508@gmail.com>
2021-08-17 19:50:34 +0530

    added single generics APIView

--- status/urls.py
+++ status/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from . import views
 
 app_name = 'status'
 
 urlpatterns = [
-
+    path('', views.StatusAPIView.as_view()),
+    path('create/', views.StatusCreateAPIView.as_view()),
+    # path('<int:pk>/', views.StatusDetailsAPIView.as_view()),
+    path('<int:id>/', views.StatusDetailsAPIView.as_view()),
+    path('<int:id>/update/', views.StatusUpdateAPIView.as_view()),
+    path('<int:id>/delete/', views.StatusDeleteAPIView.as_view())
 ]
--- status/views.py
+++ status/views.py
@@ -1,3 +1,63 @@
 from django.shortcuts import render
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework.response import Response
+from status.api.serializers import StatusSerializer
+from .models import Status
 
-# Create your views here.
+
+class StatusListSearchAPIView(APIView):
+    permission_classes = []
+    authentication_classes = []
+
+    def get(self, request):
+        qs = Status.object.all()
+        serializer = StatusSerializer(qs, many=True)
+        return Response(serializer.data)
+
+
+class StatusAPIView(generics.ListAPIView):
+    # queryset = Status.object.all()
+    serializer_class = StatusSerializer
+
+    def get_queryset(self):
+        qs = Status.object.all()
+        query = self.request.GET.get('q', None)
+
+        if query:
+            qs = qs.filter(content__icontains=query)
+
+        return qs
+
+
+class StatusCreateAPIView(generics.CreateAPIView):
+    queryset = Status.object.all()
+    serializer_class = StatusSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class StatusDetailsAPIView(generics.RetrieveAPIView):
+    queryset = Status.object.all()
+    serializer_class = StatusSerializer
+
+    # lookup_field = 'id'
+
+    # either get_object method or lookup_field
+    def get_object(self, *args, **kwargs):
+        return Status.object.get(pk=self.kwargs.get('id'))
+
+
+class StatusUpdateAPIView(generics.UpdateAPIView):
+    queryset = Status.object.all()
+    serializer_class = StatusSerializer
+    lookup_field = 'id'
+
+
+class StatusDeleteAPIView(generics.DestroyAPIView):
+    queryset = Status.object.all()
+    serializer_class = StatusSerializer
+    lookup_field = 'id'
